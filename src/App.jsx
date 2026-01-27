a7cb40f91239163ee1b7e1d8ee312eafe45a5441
Esther Inyang <estherinyang07@gmail.com>
2026-01-27 17:09:05 +0100

    Updated logo

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import "./App.css";
 const App = () => {
   return (
     <div className="text-white flex flex-col gap-3 items-center justify-center min-h-screen min-w-[320px]">
-      <h2 className="text-2xl font-medium">
-        {" "}
+      <h2 className="text-center text-lg md:text-xl lg:text-2xl font-medium">
         Hi, it's Esther <span className="wave-hand">👋</span>
       </h2>
 
-      <h1 className="text-6xl font-extrabold">Your Software Engineer!</h1>
-      <h2 className="text-base font-light">
+      <h1 className="text-2xl md:text-4xl lg:text-6xl font-extrabold">
+        Your Software Engineer!
+      </h1>
+      <h2 className="text-center text-sm lg:text-base font-light">
         &lt;Portfolio under construction... /&gt;
       </h2>
     </div>
