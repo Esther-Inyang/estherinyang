46d2f0d7fd938f7edf5cbaef3b5b2b9039ede6c0
Esther Inyang <estherinyang07@gmail.com>
2026-01-27 15:19:47 +0100

    First Portfolio Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="text-white flex flex-col gap-3 items-center justify-center min-h-screen min-w-[320px]">
+      <h2 className="text-2xl font-medium">
+        {" "}
+        Hi, it's Esther <span className="wave-hand">👋</span>
+      </h2>
+
+      <h1 className="text-6xl font-extrabold">Your Software Engineer!</h1>
+      <h2 className="text-base font-light">
+        &lt;Portfolio under construction... /&gt;
+      </h2>
+    </div>
+  );
+};
+
+export default App;
