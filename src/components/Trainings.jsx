ffe1eec4be29b7820b24082f4631ad5d336465f9
Esther Inyang <estherinyang07@gmail.com>
2026-04-04 00:19:27 +0100

    Changed image extension

--- src/components/Trainings.jsx
+++ src/components/Trainings.jsx
@@ -1,5 +1,5 @@
 import reactCert from "../assets/react-cert.jpg";
-import copilotCert from "../assets/copilotcert.png";
+import copilotCert from "../assets/copilotcert.jpg";
 import uxCert from "../assets/ux-cert.jpeg";
 
 const Training = () => {
